b36b319dd01e65cc31340b356f03014a6341ac12
Juan Carlos M Giner <jcmginer@gmail.com>
2022-12-01 13:43:02 +0100

    feat(react-hook-form): add properties to a form using react-hook-form library

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
+import { useForm } from "react-hook-form";
 import Header from "./ui/Header/Header";
+
 
 let renderedCount = 0;
 export const App = () => {
-
+  const { register, formState: { errors, dirtyFields, touchedFields, isSubmitted }, reset, handleSubmit, watch } = useForm({
+    defaultValues: {
+      /* firstName: 'Juan Carlos',
+      hobbies: 'Cars',
+      favouriteSeries: 'Patata' */
+    }
+  });
+  const onSubmit = data => console.log(data);
   renderedCount++;
 
+  const watchFirstName = watch('firstName')
+  /*   console.log('Touch', touchedFields);
+    console.log('Dirty', dirtyFields);
+    console.log('Submitted', isSubmitted);
+   */  /* console.log('Error', errors); */
   return (
     <>
-      <Header renderedCount={renderedCount}/>
+      <Header renderedCount={renderedCount} />
       <h1>Login</h1>
-      <form>
-        <input name='firstName' placeholder="First name" />
-        <input name='hobbies' placeholder="i.e. coding every day 🤩" />
-        <input name='favourite series' placeholder="Breaking bad" />
-        <button type="submit">Submit</button>
+      <form onSubmit={handleSubmit(onSubmit)} >
+        <input {...register("firstName", {
+          required: true, minLength: {
+            value: 4,
+            message: 'Minimum length is 4'
+          }
+        })} placeholder="First name" />
+        <p>{watchFirstName}</p>
+        {errors?.firstName && <p>{errors.firstName?.message}</p>}
+        <input {...register("hobbies", { required: true })} placeholder="i.e. coding every day 🤩" />
+        <input {...register("favouriteSeries")} placeholder="Breaking bad" />
+        <button type="submit" onClick={() => reset({
+          firstName: 'Juan Carlos',
+          hobbies: 'Cars',
+          favouriteSeries: 'Patata'
+        })}>Submit</button>
       </form>
     </>
   );
